352252667cb43e64f84b82f54f9dd5678d9625c4
jayanth920 <jayanthgajula92004@gmail.com>
2022-12-02 14:43:14 -0600

    added 1 mymodel

--- backend.js
+++ backend.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const { default: mongoose } = require('mongoose')
 const app = express()
 
 
 
 app.use(express.urlencoded({extended:true}))
+
 
 app.listen(4000,() => {
     console.log('app is listening on port 4000')
